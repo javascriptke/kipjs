c2d0fd9efc52b8c33a74096edaa3d2160bfda2b8
timothymugodev <timomugo01@gmail.com>
2023-03-08 12:43:58 +0300

    added chalk and fixed eslint

---
+++ packages/cli/constants.ts
@@ -0,0 +1,8 @@
+export const CONSTANTS = {
+    PACKAGE_NAME: '@kipjs/cli',
+    PACKAGE_VERSION: '0.0.1',
+    PACKAGE_DESCRIPTION: 'Kip CLI',
+    PACKAGE_USAGE: '[options]',
+}
+
+export type ConstantTypes = typeof CONSTANTS;
--- packages/cli/index.ts
+++ packages/cli/index.ts
@@ -1,5 +1,31 @@
-export default function helloCli(): string {
-  return 'Hello from cli';
+/* eslint-disable import/no-extraneous-dependencies */
+import chalk from 'chalk';
+import * as commander from 'commander';
+import { CONSTANTS } from './constants';
+
+const handleSigTerm = () => {
+  process.exit(0);
+};
+
+process.on('SIGTERM', handleSigTerm);
+process.on('SIGINT', handleSigTerm);
+
+
+const program = new commander.Command(CONSTANTS.PACKAGE_NAME)
+  .version(CONSTANTS.PACKAGE_VERSION)
+  .description(CONSTANTS.PACKAGE_DESCRIPTION)
+  .usage('[options]')
+  .option('-v, --version', 'output the version number')
+  .option(chalk('green -h, --help'), 'output usage information');
+
+const options = program.opts();
+if (options.version) {
+  console.log(CONSTANTS.PACKAGE_VERSION);
+  process.exit(0);
+}
+if (options.help) {
+  program.help();
+  process.exit(0);
 }
 
-helloCli();
+program.parse(process.argv);
